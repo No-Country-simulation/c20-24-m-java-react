0e642b2744f48f900f3e50575e60b782e57d6c84
Alejandro E Abalos F <alejandroexe96@gmail.com>
2024-08-27 17:13:41 -0300

    create page start

---
+++ front-end/src/components/mainStart/MainStart.jsx
@@ -0,0 +1,9 @@
+const MainStart = () => {
+    return (
+        <div className="flex  flex-col items-center justify-between p-10">
+            <h1 className="text-6xl font-bold ">Lorem ipsum dolor sit amet</h1>
+        </div>
+    )
+}
+
+export default MainStart;
