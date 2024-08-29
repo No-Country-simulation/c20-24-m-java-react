cfef26e600f07588821f143eabeb00dfab952a11
Luciano <lucianomolina970@gmail.com>
2024-08-29 11:23:22 -0300

    Documentation of app
    ---

--- Back-End/demo/src/main/java/com/example/demo/controller/RecipeController.java
+++ Back-End/demo/src/main/java/com/example/demo/controller/RecipeController.java
@@ -3,11 +3,18 @@ package com.example.demo.controller;
 import com.example.demo.dto.RecipeDto;
 import com.example.demo.exception.RecipeNotFoundExepcion;
 import com.example.demo.service.RecipeService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(name = "Recipes", description = "Manage all endpoints about Recipes")
 @RestController
 @RequestMapping("/recipes")
 @RequiredArgsConstructor
@@ -15,12 +22,48 @@ public class RecipeController {
 
     private final RecipeService recipeService;
 
+    @Operation(
+            summary = "Create a new recipe.",
+            description = "Creates a new recipe with the provided details."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "201", description = "Recipe created successfully",
+                    content = {
+                            @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = RecipeDto.class))
+                    }),
+            @ApiResponse(responseCode = "403", description = "Forbidden access to this resource", content = {
+                    @Content}),
+            @ApiResponse(responseCode = "400", description = "Invalid request body", content = {
+                    @Content}),
+            @ApiResponse(responseCode = "500", description = "Internal Server Error", content = {
+                    @Content})
+    })
     @PostMapping("/save")
     public ResponseEntity<RecipeDto> createRecipe(@RequestBody RecipeDto recipeDto) {
         RecipeDto saveRecipe= recipeService.createRecipe(recipeDto);
         return new ResponseEntity<>(saveRecipe, HttpStatus.CREATED);
     }
 
+    @Operation(
+            summary = "Find a recipe by ID.",
+            description = "Finds a recipe by its unique identifier."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200", description = "recipe found",
+                    content = {
+                            @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = RecipeDto.class))
+                    }),
+            @ApiResponse(responseCode = "403", description = "Forbidden access to this resource", content = {
+                    @Content}),
+            @ApiResponse(responseCode = "404", description = "Recipe not found", content = {
+                    @Content}),
+            @ApiResponse(responseCode = "500", description = "Internal Server Error", content = {
+                    @Content})
+    })
     @GetMapping("/search/{id}")
     public ResponseEntity<?> findRecipeById(@PathVariable("id") Long id) {
         try {
