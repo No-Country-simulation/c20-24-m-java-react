b656503fcc073053a7cf91ecb1b09bdf91de89df
Hector Duarte <hectorduarte240.hd@gmail.com>
2024-08-29 00:01:13 -0300

    feat: ud

---
+++ front-end/src/components/button/Button.jsx
@@ -0,0 +1,5 @@
+const Button = ({className,children}) => {
+  return (<><button className={className}>{children}</button></>) 
+}
+
+export default Button;
---
+++ front-end/src/components/imgMainStar/ImgMainStar/ImgMainStar.jsx
@@ -0,0 +1,9 @@
+function ImgMainStar () {
+    return (
+    <>
+        <div className="">
+            
+        </div>
+    </>
+    )
+}
