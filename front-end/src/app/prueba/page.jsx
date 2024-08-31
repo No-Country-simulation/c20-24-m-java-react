2850a7f19d73bcedcc4edb4fe690248fe669b5bb
Alejandro E Abalos F <alejandroexe96@gmail.com>
2024-08-30 23:31:33 -0300

    update-modal

--- front-end/src/app/prueba/page.jsx
+++ front-end/src/app/prueba/page.jsx
@@ -1,8 +1,11 @@
 import ModalLoginMainStart from "@/components/modalLoginMainStart/ModalLoginMainStart";
+// import { useState } from "react";
 
 
 
 export default function Prueba() {
+    // const [isModalVisible, setIsModalVisible] = useState(false);
+    
     return (
         <div>
           <button class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
--- front-end/src/components/modalLoginMainStart/ModalLoginMainStart.jsx
+++ front-end/src/components/modalLoginMainStart/ModalLoginMainStart.jsx
@@ -1,34 +1,54 @@
 import Image from "next/image";
 
 const ModalLoginMainStart = () => {
+    const p = "px-28 py-32";
     return (
         <section id="modal_main" className="fixed top-0 left-0 right-0 bottom-0 bg-black bg-opacity-50 flex">
-            <div id="modal_container" className="m-auto w-4/5 max-w-screen-xl max-h-full bg-slate-300 rounded-md px-28 py-32">
-                <div className="grid grid-cols-2 gap-24">
-                    <form action="">
-                        <div class="relative z-0 w-full mb-5 group">
-                            <input type="email" name="floating_email" id="floating_email" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
-                            <label for="floating_email" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Email address</label>
-                        </div>
-
-                        <div >
-                            <div className="relative block">
-                                <p className="absolute left-3 top-2.5 transition-all duration-[1s]  
-                                
-                                top-[-11px]
-                                ">Email</p>
-                                <input type="email"  className="block w-4/5 h-11 px-2 border-solid border-2 border-slate-900 rad rounded-lg bg-clip-padding"/>
+            <div id="modal_container" className="m-auto w-3/5 max-w-screen-xl max-h-full bg-slate-300 rounded-md ">
+                {/* <div className="grid grid-cols-2 gap-3"> */}
+                <div className="flex">
+                    <div className="px-14 py-20 bg-white flex w-4/5 flex-col  items-center  ">
+                        <form action="" className=" ">
+                            <div class="relative z-0  mb-6 group ">
+                                <input type="email" name="floating_email" id="floating_email" className="block px-2 h-[48px]  text-black py-2.5  w-[319px]
+                                 text-sm  bg-transparent border-2  border-gray-300 appearance-none  dark:border-gray-600  focus:outline-none focus:ring-0 focus:border-green-600 peer rounded-lg" placeholder=" " required />
+                                <label for="floating_email" class="peer-focus:text-sm  absolute text-sm  duration-300 transform -translate-y-6 scale-75 top-[14px]  origin-[0] peer-focus:start-3 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-green-600 peer-focus:dark:text-green-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 bg-white left-3 px-1 pointer-events-none">Nombre</label>
                             </div>
+                            <div class="relative z-0  mb-6 group ">
+                                <input type="email" name="floating_email" id="floating_email" className="block px-2 h-[48px]  text-black py-2.5  w-[319px] text-sm  bg-transparent border-2  border-gray-300 appearance-none  dark:border-gray-600  focus:outline-none focus:ring-0 focus:border-green-600 peer rounded-lg" placeholder=" " required />
+                                <label for="floating_email" class="peer-focus:text-sm  absolute text-sm  duration-300 transform -translate-y-6 scale-75 top-[14px]  origin-[0] peer-focus:start-3 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-green-600 peer-focus:dark:text-green-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 bg-white left-3 px-1 pointer-events-none">Correo eletronico</label>
+                            </div>
+                            <div class="relative z-0  mb-6 group ">
+                                <input type="email" name="floating_email" id="floating_email" className="block px-2 h-[48px]  text-black py-2.5  w-[319px] text-sm  bg-transparent border-2  border-gray-300 appearance-none  dark:border-gray-600  focus:outline-none focus:ring-0 focus:border-green-600 peer rounded-lg" placeholder=" " required />
+                                <label for="floating_email" class="peer-focus:text-sm  absolute text-sm  duration-300 transform -translate-y-6 scale-75 top-[14px]   origin-[0] peer-focus:start-3 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-green-600 peer-focus:dark:text-green-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 bg-white left-3 px-1 select-none pointer-events-none">Contraseña</label>
+                            </div>
+                            
+                            
+                            <button class=" my-5  w-[319px] h-[48px] bg-[#F27B13] hover:bg-orange-300 hover:shadow-xl text-black font-semibold hover:text-black py-2 px-4 border hover:border-transparent rounded-lg">
+                            Registrarse
+                            </button>
+                            
+                        </form>
+                        <p className="mb-4 mt-2">o registrate con </p>
+                        <button class="   w-[319px] h-[48px] bg-slate-50 hover:bg-[#F27B13] shadow-xl hover:shadow-none text-black font-semibold hover:text-black py-2 px-4 border hover:border-transparent rounded-lg">
+                            Registrarse con google
+                            </button>
+
+                        <p className="mt-12">¿Ya tenes cuenta? <a href="" className="text-[#F27B13]">Iniciar sesión</a></p>
+                    </div>
+
+                    <div className="text-white bg-[#160852] w-full px-14 py-16 ">
+                        <div>
+
                         </div>
-                    </form>
-                    <div >
-                        <h2 className="text-2xl font-bold text-center">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rerum nobis </h2>
-                        <p className="text-center mt-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita eum cupiditate tempore, reprehenderit magni nihil quos </p>
+                        <h2 className="text-2xl font-bold text-center">¡Únete a nuestra comunidad de apasionados por la cocina! </h2>
+                        <p className="text-center mt-4">¡Ser parte es fácil y gratuito! </p>
                         <div className="flex  flex-col items-center justify-between ">
-                            <Image  className="h-64 mt-14"
-                                width={2000}
+                            <Image  className="w-3/4 mt-5"
+                                width={3000}
                                 height={2000} 
-                                src="https://w.wallhaven.cc/full/j8/wallhaven-j8mo65.jpg" alt="picture" />
+                                src="/img/Iniciar sesion ilustracion.svg" alt="picture" />
+                            
                         </div>
                     </div>
                 </div>
