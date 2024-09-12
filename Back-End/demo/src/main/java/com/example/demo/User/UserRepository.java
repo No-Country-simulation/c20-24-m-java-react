c034bb3344ea1645325c221e728829d59e1a2612
simonineloy123 <simonineloy123@gmail.com>
2024-09-12 03:02:30 -0300

    Add validations for login and register users

--- Back-End/demo/src/main/java/com/example/demo/User/UserRepository.java
+++ Back-End/demo/src/main/java/com/example/demo/User/UserRepository.java
@@ -8,5 +8,9 @@ public interface UserRepository extends JpaRepository <User, Integer> {
 
     Optional<User> findByUsername(String username);
 
+    boolean existsByUsername(String username);
+
+    boolean existsByEmail(String email);
+
 
 }
