213c9c47249c381ca26bb7cca5b190d5f5c1f960
Eric M <ericmerel07@gmail.com>
2024-09-13 19:26:01 -0500

    Solucione los errores al tratar de unir los proyectos

--- Back-End/demo/src/main/java/com/example/demo/model/Recipe.java
+++ Back-End/demo/src/main/java/com/example/demo/model/Recipe.java
@@ -1,25 +1,48 @@
 package com.example.demo.model;
 
 import jakarta.persistence.*;
-import lombok.Data;
+import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
-@Table(name = "recipe")
-@Data
+@Table(name = "recipes")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Recipe {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
     private String description;
     private String ingredients;
-    private String instruction;
-
+    private String instructions;
     @CreationTimestamp
-    @Column(updatable = false)
+    @Column(nullable = false, updatable = false)
     private LocalDateTime dateCreation;
+    @Enumerated(EnumType.STRING)
+    private Category category;
+    private String time;
+    private String commensal;
+    private String amount;
+    @ElementCollection
+    @CollectionTable(name = "recipe_images", joinColumns = @JoinColumn(name = "recipe_id"))
+    @Column(name = "image_url")
+    private List<String> imageUrls = new ArrayList<>();
+
+    @OneToMany(mappedBy = "recipe", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Calification> califications;
+
+
+//    @OneToMany(mappedBy = "recipe", cascade = CascadeType.ALL, orphanRemoval = true)
+//    private List<Comment> comments = new ArrayList<>();
+
 }
 
