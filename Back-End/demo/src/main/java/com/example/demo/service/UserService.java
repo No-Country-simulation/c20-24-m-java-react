f43e46e5baa48c7645acf659cb03277a6542e0a7
Luciano <lucianomolina970@gmail.com>
2024-10-01 23:13:45 -0300

    + changes in security corss
    ---

--- Back-End/demo/src/main/java/com/example/demo/service/UserService.java
+++ Back-End/demo/src/main/java/com/example/demo/service/UserService.java
@@ -2,6 +2,7 @@ package com.example.demo.service;
 
 import com.example.demo.User.User;
 import com.example.demo.User.UserRepository;
+import com.example.demo.exception.RecipeNotFoundExepcion;
 import com.example.demo.exception.UserNotFoundExepcion;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -12,7 +13,7 @@ public class UserService {
     private final UserRepository userRepository;
 
     public User findByUsername (String userName){
-        return userRepository.findByUsername(userName).orElseThrow(() -> new UserNotFoundExepcion(STR."This Recipe Does Not Exist with that UserName: \{userName}"));
+        return userRepository.findByUsername(userName).orElseThrow(() -> new UserNotFoundExepcion("This Recipe Does Not Exist with that UserName: " + userName));
     }
 
 }
