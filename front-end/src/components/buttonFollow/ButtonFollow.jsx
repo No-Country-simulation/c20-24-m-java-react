a99a8eb8591bc79737104773e6e470514cc41ca4
Alejandro E Abalos F <alejandroexe96@gmail.com>
2024-10-29 21:41:42 -0300

    fix: Button Edit Profile and Follow

---
+++ front-end/src/components/buttonFollow/ButtonFollow.jsx
@@ -0,0 +1,9 @@
+const ButtonFollow = ({ user }) => {
+  return (
+    <button className=" my-5  w-[183px] h-[48px] bg-[#7DA626] hover:bg-[#160852] hover:shadow-xl text-black font-semibold hover:text-white  px-4 border hover:border-transparent rounded-2xl">
+      Seguir
+    </button>
+  );
+};
+
+export default ButtonFollow;
--- front-end/src/components/userProfile/UserProfile.jsx
+++ front-end/src/components/userProfile/UserProfile.jsx
@@ -14,6 +14,9 @@ import { deletePageKeyPegeScrollGeneric } from '@/redux/pegeScrollGeneric/pegeSc
 import { setResetState } from '@/redux/resetStatePage/resetStatePageSlice';
 import ButtonUploadRecipe from '../buttonUploadRecipe/buttonUploadRecipe';
 import { useUserContext } from '../UserProvider';
+import ButtonEditProfile from '../buttonEditProfile/buttonEditProfile';
+import { Button } from '@material-tailwind/react';
+import ButtonFollow from '../buttonFollow/ButtonFollow';
 
 const BACK_API_URL = process.env.NEXT_PUBLIC_API_URL;
 
@@ -57,66 +60,89 @@ const UserProfile = ({ onClose, userId, username, imageUser }) => {
   };
 
   return (
-    <div className="flex flex-col justify-center items-center ">
-      <div
-        onClick={handleBackDeletePage}
-        className="fixed top-[12px] left-[30px] cursor-pointer"
-      >
-        <IoArrowBackCircleOutline className="w-[80px] h-[80px] text-white drop-shadow-[0_1px_1px_rgba(1,1,1,1.2)] " />
+    <>
+      <div className=" h-[270px] bg-black ">
+        <h1>Hola</h1>
       </div>
-      <div className="flex justify-center items-center  mt-1 bg-[#fff8f2] w-[252px] h-[252px] rounded-[50%] shadow-md ">
-        {imageUser ? (
-          <Image
-            className=" w-[250px] h-[250px] rounded-[50%]"
-            width={60}
-            height={60}
-            src="/img/Registro ilustracion.svg"
-            alt="picture"
-          />
-        ) : (
-          <FaUser className="rounded-full w-[240px] h-[240px]" />
-        )}
-      </div>
-      <div className="my-5">
-        <p className="text-[25px] text-center w-[250px] h-[[calc(var(--vh, 1vh) * 100)]] overflow-hidden text-ellipsis leading-tight font-semibold">
-          {username || 'Username'}
-        </p>
-      </div>
-      <div className="my-5 ml-5 flex justify-center  items-center w-[250px]">
-        <MapPin />
-        <p className="flex items-center ml-1 text-[14px] text-start w-[150px] h-[[calc(var(--vh, 1vh) * 100)]]  text-ellipsis leading-tight ">
-          Rosario, Argentina
-        </p>
-      </div>
-      <div>
-        <p className="text-[14px] text-center w-[300px] h-[[calc(var(--vh, 1vh) * 100)]]  text-ellipsis leading-tight ">
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officiis harum
-          sit libero quia deserunt vitae praesentium saepe culpa provident quos!
-          Dolores fugit perspiciatis tempore sint provident voluptatum vero expedita
-          excepturi.
-        </p>
-      </div>
-      <div className="my-5 flex flex-row justify-center items-center">
-        <Instagram className="mx-1" />
-        <Image alt="facebook" width={24} height={24} src="/icons/bi--facebook.svg" />
-        <Globe className="mx-1" />
-      </div>
-      <div className="flex flex-row justify-center items-center bg-white rounded-3xl border-[1px] border-green-400 border-solid shadow-md p-2">
-        <div className="mx-1 text-center w-[100px] ">
-          <p className="font-semibold h-4">50</p>
-          <p className="text-[14px]">Seguiendo</p>
-        </div>
-        <div className="mx-1 text-center w-[100px] ">
-          <p className="font-semibold h-4">50</p>
-          <p className="text-[14px]">Seguidores</p>
-        </div>
-      </div>
-      {username === user.username ? (
-        <div className="flex justify-center pt-11">
-          <ButtonUploadRecipe />
+      {username === user?.username ? (
+        <div className="absolute z-100 top-[12px] right-[30px] cursor-pointer">
+          <ButtonEditProfile />
         </div>
       ) : null}
-    </div>
+
+      <div className="w-[330px] h-[600px] fixed z-10 top-[150px] mx-[100px]">
+        <div className="flex flex-col justify-center items-center ">
+          <div
+            onClick={handleBackDeletePage}
+            className="fixed top-[12px] left-[30px] cursor-pointer"
+          >
+            <IoArrowBackCircleOutline className="w-[80px] h-[80px] text-white drop-shadow-[0_1px_1px_rgba(1,1,1,1.2)] " />
+          </div>
+
+          <div className="flex justify-center items-center  mt-1 bg-[#fff8f2] w-[252px] h-[252px] rounded-[50%] shadow-md ">
+            {imageUser ? (
+              <Image
+                className=" w-[250px] h-[250px] rounded-[50%]"
+                width={60}
+                height={60}
+                src="/img/Registro ilustracion.svg"
+                alt="picture"
+              />
+            ) : (
+              <FaUser className="rounded-full w-[240px] h-[240px]" />
+            )}
+          </div>
+          <div className="my-5">
+            <p className="text-[25px] text-center w-[250px] h-[[calc(var(--vh, 1vh) * 100)]] overflow-hidden text-ellipsis leading-tight font-semibold">
+              {username || 'Username'}
+            </p>
+          </div>
+          <div className="my-5 ml-5 flex justify-center  items-center w-[250px]">
+            <MapPin />
+            <p className="flex items-center ml-1 text-[14px] text-start w-[150px] h-[[calc(var(--vh, 1vh) * 100)]]  text-ellipsis leading-tight ">
+              Rosario, Argentina
+            </p>
+          </div>
+          <div>
+            <p className="text-[14px] text-center w-[300px] h-[[calc(var(--vh, 1vh) * 100)]]  text-ellipsis leading-tight ">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officiis
+              harum sit libero quia deserunt vitae praesentium saepe culpa provident
+              quos! Dolores fugit perspiciatis tempore sint provident voluptatum vero
+              expedita excepturi.
+            </p>
+          </div>
+          <div className="my-5 flex flex-row justify-center items-center">
+            <Instagram className="mx-1" />
+            <Image
+              alt="facebook"
+              width={24}
+              height={24}
+              src="/icons/bi--facebook.svg"
+            />
+            <Globe className="mx-1" />
+          </div>
+          <div className="flex flex-row justify-center items-center bg-white rounded-3xl border-[1px] border-green-400 border-solid shadow-md p-2">
+            <div className="mx-1 text-center w-[100px] ">
+              <p className="font-semibold h-4">50</p>
+              <p className="text-[14px]">Seguiendo</p>
+            </div>
+            <div className="mx-1 text-center w-[100px] ">
+              <p className="font-semibold h-4">50</p>
+              <p className="text-[14px]">Seguidores</p>
+            </div>
+          </div>
+          <div className="flex justify-center pt-11">
+            {username === user?.username ? (
+              <ButtonUploadRecipe />
+            ) : (
+              <>
+                <ButtonFollow user={user} />
+              </>
+            )}
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
